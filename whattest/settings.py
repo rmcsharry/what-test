f08bcf022c757c2d01763f5ce76f330213aa238b
Richard McSharry <rmcsharry@gmail.com>
2023-11-26 14:34:40 +0100

    turn off cors

--- whattest/settings.py
+++ whattest/settings.py
@@ -175,14 +175,14 @@ SECURE_SSL_REDIRECT = os.environ.get('SECURE_SSL_REDIRECT') != "False"
 CORS_ALLOW_CREDENTIALS = True
 CSRF_COOKIE_NAME = "csrftoken"
 
-# CORS_ORIGIN_ALLOW_ALL = True
-
 CORS_ALLOW_HEADERS = default_headers
 
 # TODO - use env vars once you know how to add them on DIVIO
 
 CORS_ALLOWED_ORIGINS = ['http://localhost', 'http://127.0.0.1', 'https://what-fe-stage.us.aldryn.io:443', 'https://what-fe.us.aldryn.io:443']
 CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1', 'https://what-fe-stage.us.aldryn.io:443', 'https://what-fe.us.aldryn.io:443']
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ## User model
 AUTH_USER_MODEL = 'user_api.AppUser'
